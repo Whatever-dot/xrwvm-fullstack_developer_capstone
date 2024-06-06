ea09ed1d963841388f57890c762bc7fa3e2da77a
Whatever-dot <jnamg1998@gmail.com>
2024-06-06 10:02:23 -0400

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,10 +14,10 @@ sentiment_analyzer_url = os.getenv(
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
-        for key, value in kwargs.items():
-            params = params + key + "=" + value + "&"
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
 
-    request_url = backend_url+endpoints+"?"+params
+    request_url = backend_url+endpoint+"?"+params
 
     print("GET from {}".format(request_url))
     try:
@@ -41,7 +41,7 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = request.post(request_url,json=data_dict)
+        response = requests.post(request_url,json=data_dict)
         print(response.json())
         return respons.json()
     except:
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -102,7 +102,8 @@ def get_dealerships(request, state="All"):
     return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
@@ -115,7 +116,7 @@ def get_dealer_reviews(request,dealer_id):
         return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
-def get_dealer_details(request,dealer_id):
+def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
